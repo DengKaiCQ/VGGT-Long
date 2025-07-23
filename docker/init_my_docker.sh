3eeb84d68d588f0ee380b0d0b0e9e8d6e03247b7
ZixunH <zixun@berkeley.edu>
2025-07-23 16:51:56 -0700

    added docker support

---
+++ docker/init_my_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash          
+PROJECT_DIR="../"
+DATASET_DIR="/mnt/raid/dataset"
+
+sudo docker remove -f vggt-long
+
+echo "mount projects: $PROJECT_DIR --> vggt-long:/home"
+echo "mount datasets: $DATASET_DIR --> vggt-long:/media"
+
+sudo docker run --name vggt-long -t -d --gpus 'all,"capabilities=compute,utility,graphics"' \
+    --volume="$DATASET_DIR:/media:rw" \
+    --volume="$PROJECT_DIR:/home:rw" \
+    --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY --env=QT_X11_NO_MINTSHM=1 \
+    vggt-long:latest
+
+xhost -si:localuser:root
+xhost si:localuser:root
+
+sudo docker exec -it vggt-long /bin/bash --login
---
+++ docker/run_my_docker.sh
@@ -0,0 +1,1 @@
+sudo docker exec -it vggt-long /bin/bash --login
